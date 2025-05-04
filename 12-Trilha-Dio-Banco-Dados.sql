bd44a83181f41187d1de2874079913744229ab7c
Herick Prazeres <52898131+HerickPrazeres82@users.noreply.github.com>
2025-05-03 22:33:00 -0300

    Update 12-Trilha-Dio-Banco-Dados.sql
    Adicionando enunciado do exercício

--- 12-Trilha-Dio-Banco-Dados.sql
+++ 12-Trilha-Dio-Banco-Dados.sql
@@ -1,3 +1,4 @@
+--12) Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
 select f.Nome, a.PrimeiroNome, a.UltimoNome, ef.Papel
 from Filmes as f
 join ElencoFilme as ef
